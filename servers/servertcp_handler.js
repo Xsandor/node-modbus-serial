3ffde3c87178c1c75bebaf8301667d55d0f8e57d
Alexander Schmidt <alex@karlborgselkontroll.se>
2023-11-22 14:03:09 +0100

    Fixes

--- servers/servertcp_handler.js
+++ servers/servertcp_handler.js
@@ -1009,9 +1009,11 @@ function _handleReadFile(requestBuffer, vector, unitID, callback) {
 
     if (fileNumber === 2015 && recordLength === 8 && recordNumber === 0 && referenceType === 7) {
         const responseBuffer = Buffer.alloc(5 + recordLength * 2 + 2);
+        responseBuffer.writeUInt8(12, 1);
+        responseBuffer.writeUInt8(11, 2);
         responseBuffer.writeUInt8(recordLength, 3);
         responseBuffer.writeUInt8(referenceType, 4);
-        responseBuffer.write("084B8014 EKC2/3", 5, recordLength, "ascii");
+        responseBuffer.write("084B8014 EKC2/3", 5, "ascii");
         callback(null, responseBuffer);
     } else {
         callback(1);
